ce0e395991c3cc40ca68c91f5b311c96f5eb5c65
Calum Cook <calumcook@Calums-Air.broadband>
2022-08-23 00:52:11 +0100

    basic structure made - one test file

--- src/main/java/com/example/gameService/Components/DataLoader.java
+++ src/main/java/com/example/gameService/Components/DataLoader.java
@@ -1,4 +1,84 @@
 package com.example.gameService.Components;
 
-public class DataLoader {
+import com.example.gameService.Models.Characters.Enemy;
+import com.example.gameService.Models.Characters.Player;
+import com.example.gameService.Models.Encounters.BattleEncounter;
+import com.example.gameService.Models.Encounters.BossEncounter;
+import com.example.gameService.Models.Encounters.LootEncounter;
+import com.example.gameService.Models.Weapons.Weapon;
+import com.example.gameService.Repositories.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    @Autowired
+    BattleEncounterRepository battleEncounterRepository;
+
+    @Autowired
+    BossEncounterRepository bossEncounterRepository;
+
+    @Autowired
+    EnemyRepository enemyRepository;
+
+    @Autowired
+    LootEncounterRepository lootEncounterRepository;
+
+    @Autowired
+    PlayerRepository playerRepository;
+
+    @Autowired
+    WeaponRepository weaponRepository;
+
+    public DataLoader() {
+
+    }
+
+    public void run(ApplicationArguments args) {
+
+        Weapon longSword = new Weapon("Long Sword", 25);
+        weaponRepository.save(longSword);
+
+        Weapon shortSword = new Weapon("Short Sword", 18);
+        weaponRepository.save(shortSword);
+
+        Weapon battleAxe = new Weapon("Battle Axe", 30);
+        weaponRepository.save(battleAxe);
+
+        Weapon handAxe = new Weapon("Hand Axe", 14);
+        weaponRepository.save(handAxe);
+
+        Weapon wakizashi = new Weapon("Wakizashi", 28);
+        weaponRepository.save(wakizashi);
+
+        Weapon kodachi = new Weapon("Kodachi", 18);
+        weaponRepository.save(kodachi);
+
+        Enemy Ogre = new Enemy("Og the Ogre", 100, 50, 1, shortSword);
+        enemyRepository.save(Ogre);
+
+        Enemy Troll = new Enemy("Tom the Troll", 100, 50, 1, shortSword);
+        enemyRepository.save(Troll);
+
+        Enemy Bat = new Enemy("Bob the Bat", 100, 50, 1, shortSword);
+        enemyRepository.save(Bat);
+
+        Enemy Slug = new Enemy("Sam the Slug", 100, 50, 1, shortSword);
+        enemyRepository.save(Slug);
+
+        Player kevin = new Player("Kevin", 100, 10,1, battleAxe);
+        playerRepository.save(kevin);
+
+        BattleEncounter battle1 = new BattleEncounter("First battle", Slug);
+        battleEncounterRepository.save(battle1);
+
+        LootEncounter loot1 = new LootEncounter("First swag", kodachi);
+        lootEncounterRepository.save(loot1);
+
+        BossEncounter boss1 = new BossEncounter("First boss", Bat);
+        bossEncounterRepository.save(boss1);
+    }
 }
