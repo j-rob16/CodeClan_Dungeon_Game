f02ac16d0779508a894ca5f78836dfa7ddfe0163
James R <jimmer1609@gmail.com>
2022-08-23 21:13:32 +0900

    encounter rooms tests added

--- src/test/java/com/example/gameService/EncounterTests/BattleEncounterTest.java
+++ src/test/java/com/example/gameService/EncounterTests/BattleEncounterTest.java
@@ -4,6 +4,9 @@ import com.example.gameService.Models.Characters.Enemy;
 import com.example.gameService.Models.Encounters.BattleEncounter;
 import com.example.gameService.Models.Weapons.Weapon;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BattleEncounterTest {
 
@@ -16,5 +19,15 @@ public class BattleEncounterTest {
         weapon = new Weapon("Sword", 30);
         enemy = new Enemy("Orc", 120, 50, 1, weapon);
         battleEncounter = new BattleEncounter("Pirate Battle", enemy);
+    }
+
+    @Test
+    public void canGetEncounterName() {
+        assertEquals("Pirate Battle", battleEncounter.getName());
+    }
+
+    @Test
+    public void canGetEncounterEnemy() {
+        assertEquals(enemy, battleEncounter.getEnemy());
     }
 }
