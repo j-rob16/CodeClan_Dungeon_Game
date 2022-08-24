df19361867dff03281a0241f45ea8a39ba6d0d35
Student <eriksund84@gmail.com>
2022-08-24 12:01:48 +0100

    State passed back up to main container

--- src/main/ui/src/components/Menus/CharacterOption/CharacterOption.js
+++ src/main/ui/src/components/Menus/CharacterOption/CharacterOption.js
@@ -1,12 +1,17 @@
 import styles from './styles.module.css'
 
-export const CharacterOption = ({player, onCharacterClick}) => {
+export const CharacterOption = ({player, onClick}) => {
+
+  function handleCharacterClick(){
+    onClick(player);
+  }
+
   return (
       <div className={styles.main}>
         <p>Name: {player.name}</p>
         <p>Health: {player.maxHealth}</p>
         <p>Level: {player.level}</p>
-        <button>Select Character!</button>
+        <button onClick={handleCharacterClick}>Select Character!</button>
       </div>
   )
 }
