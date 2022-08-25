45abd1fbe26cd043732beb4ffcaf64fc3bb669b8
Student <eriksund84@gmail.com>
2022-08-25 13:12:57 +0100

    Forgot to save a file

--- src/main/ui/src/components/Game/Battle/CharacterComponent/Character.js
+++ src/main/ui/src/components/Game/Battle/CharacterComponent/Character.js
@@ -3,6 +3,9 @@ import React from 'react';
 
 import { HealthBar } from '../HealthBar';
 export const Character = (entity) => {
+
+
+
     return (
       <div className={styles.main}>
        Character
