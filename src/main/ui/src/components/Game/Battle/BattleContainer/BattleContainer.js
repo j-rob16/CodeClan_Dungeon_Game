a4b223d7d82caa673e1a799c52a5b1b2a5787c34
Student <eriksund84@gmail.com>
2022-08-25 13:12:32 +0100

    I think the page is loading before the data

--- src/main/ui/src/components/Game/Battle/BattleContainer/BattleContainer.js
+++ src/main/ui/src/components/Game/Battle/BattleContainer/BattleContainer.js
@@ -2,13 +2,14 @@ import styles from './styles.module.css';
 
 import { Character, BattleMenu } from 'components';
 import { GameNarrator } from 'components';
+import React from 'react';
 
 export const BattleContainer = ({selectedCharacter}) => {
   return (
     <>
       <div className={styles.main}>
         <div className={styles.Player}>
-          <Character selectedCharacter={selectedCharacter}/>
+          <Character entity={selectedCharacter}/>
         </div>
         <div className={styles.Enemy}>
           <Character />
--- src/main/ui/src/components/Game/Battle/CharacterComponent/Character.js
+++ src/main/ui/src/components/Game/Battle/CharacterComponent/Character.js
@@ -2,13 +2,13 @@ import styles from './styles.module.css';
 import React from 'react';
 
 import { HealthBar } from '../HealthBar';
-export const Character = (selectedCharacter) => {
+export const Character = (entity) => {
     return (
       <div className={styles.main}>
        Character
        <div className = {styles.HealthBar}>
        <HealthBar />
-       <p>Name: {selectedCharacter.name}</p>
+       <p>Name: {entity.name}</p>
        </div>
       </div>
     );
