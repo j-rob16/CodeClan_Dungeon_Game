16f38be1914a341c954d604776795263566e15ac
Calum Cook <calumcook@Calums-Air.broadband>
2022-08-26 01:35:04 +0100

    initial commit

--- src/main/ui/src/components/GameContainer/GameContainer.js
+++ src/main/ui/src/components/GameContainer/GameContainer.js
@@ -6,23 +6,8 @@ import { StartMenu } from 'components/Menus';
 import { BattleContainer } from 'components/Game/Battle';
 export const GameContainer = ({gameData, selectedCharacter}) => {
 
-  // const [weaponsData, setWeaponsData] = useState([]);
-  // const [enemiesData, setEnemiesdata] = useState([]);
-  // const [battleEncounters, setBattleEncounters] = useState([]);
-
-  // async function setAllGameData() {
-  //   setWeaponsData(gameData[1]);
-  //   setEnemiesdata(gameData[2]);
-  //   setBattleEncounters(gameData[3]);
-  // }
-
-  // useEffect(() => {
-  //   setAllGameData();
-  // }, [gameData])
-
   return (
     <div className={styles.main}>
-      {/* <StartMenu/> */}
       <BattleContainer selectedCharacter={selectedCharacter} gameData={gameData}/>
     </div>
   );
