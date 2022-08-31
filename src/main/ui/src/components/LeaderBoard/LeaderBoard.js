1dc0e247bae676f5c28aa9bdc203c550e600abbe
Student <eriksund84@gmail.com>
2022-08-31 11:19:41 +0100

    end screen flow and narrator now works

--- src/main/ui/src/components/LeaderBoard/LeaderBoard.js
+++ src/main/ui/src/components/LeaderBoard/LeaderBoard.js
@@ -1,15 +1,20 @@
+import { StartGameNarrator } from 'components/Menus/StartMenu/StartGameNarrator';
 import styles from './styles.module.css';
 
 export const LeaderBoard = ({ winner, onStartClick }) => {
   return (
     <div className={styles.main}>
-      <h1 className={styles.glow}>
-        {winner.name} has completed their journey!
-      </h1>
+      {/* <h1 className={styles.glow}> */}
+      <StartGameNarrator
+        script={`${winner.name} has completed their journey.`}
+        viewModeClick={onStartClick}
+      />
+        {/* {winner.name} has completed their journey! */}
+      {/* </h1> */}
       {/* character celebrating animation here */}
-      <button className={styles.button} onClick={onStartClick}>
+      {/* <button className={styles.button} onClick={onStartClick}>
         Start Again?
-      </button>
+      </button> */}
     </div>
   );
 };
